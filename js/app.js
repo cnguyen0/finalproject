1c4738ec299480e4d236010b7ed0df5f328fd75f
cindy <cnguyen0@uw.edu>
2015-12-05 22:20:41 -0800

    ADDED TRANSITIONS. PLEASE CHCK IT OUT AND SEE IF ITS COOL 1111

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(function() {
     if (window.innerWidth > 768) {
       var nav = document.querySelector("nav");
 
-      if (window.pageYOffset > 215) {
+      if (window.pageYOffset > 400) {
         nav.style.position = "fixed";
         nav.style.top = "0px";
         nav.style.width = "100%";
@@ -25,4 +25,29 @@ $(function() {
       }
     }
   }
+
+  $(document).ready(function() {
+    $(".animsition").animsition({
+      inClass: 'fade-in',
+      outClass: 'fade-out',
+      inDuration: 800,
+      outDuration: 800,
+      linkElement: '.animsition-link',
+      // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+      loading: true,
+      loadingParentElement: 'body', //animsition wrapper element
+      loadingClass: 'animsition-loading',
+      loadingInner: '', // e.g '<img src="loading.svg" />'
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      browser: [ 'animation-duration', '-webkit-animation-duration'],
+      // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+      // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+      overlay : false,
+      overlayClass : 'animsition-overlay-slide',
+      overlayParentElement : 'body',
+      transition: function(url){ window.location.href = url; }
+    });
+  });
 });
