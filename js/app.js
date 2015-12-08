797ebbc5226cc77b41405da430d737977383c210
cindy <cnguyen0@uw.edu>
2015-12-07 23:12:21 -0800

    added footer, box shadow to navbar. and kinda started on the slide show on home page

--- js/app.js
+++ js/app.js
@@ -15,14 +15,37 @@ angular.module('WebApp', [])
 
 		        nav.style.backgroundColor = "#F1F1F1";
 		        nav.style.borderRadius = "5px";
-		    } else {
-		    	nav.style.position = "";
-		        nav.style.top = "";
+		        nav.style.boxShadow = "0px 10px 10px #939393";
+		    	} else {
+			    	nav.style.position = "";
+			        nav.style.top = "";
 
-		        nav.style.backgroundColor = "transparent";
-		        nav.style.borderRadius = "none";
-		        nav.style.border = "0px";
-      }
-    }
-		}
+			        nav.style.backgroundColor = "transparent";
+			        nav.style.borderRadius = "none";
+			        nav.style.border = "0px";
+			        nav.style.boxShadow = "";
+      			}
+    		}
+    	}
 	});
+
+	/*
+	.controller('SlideController', function($scope, $http) {
+		function CarouselDemoCtrl($scope){
+			$scope.myInterval = 3000;
+			$scope.slides = [
+				{
+				  image: 'http://lorempixel.com/400/200/'
+				},
+				{
+				  image: 'http://lorempixel.com/400/200/food'
+				},
+				{
+				  image: 'http://lorempixel.com/400/200/sports'
+				},
+				{
+				  image: 'http://lorempixel.com/400/200/people'
+				}
+			];
+		}
+	});*/
