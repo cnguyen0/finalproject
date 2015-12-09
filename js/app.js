bfbaf9061d2ea938250884ea764d4872420cb355
Mina <minatari@uw.edu>
2015-12-08 17:14:36 -0800

    fixed flashy screen issue probably

--- js/app.js
+++ js/app.js
@@ -2,30 +2,31 @@ angular.module('WebApp', [])
 	.controller('NavController', function($scope, $http) {
 		'use strict';
 		window.onscroll = changePos;
+		var bodySize = window.innerHeight;
+		console.log(bodySize);
 
 		function changePos() {
 		    var header = document.getElementById("navbar");
 		    if (window.innerWidth > 768) {
-		    	var nav = document.querySelector("nav");
+			var nav = document.querySelector("nav");
+				if (window.pageYOffset > 400 && window.innerHeight > 803) {
+					nav.style.position = "fixed";
+					nav.style.top = "0px";
+					nav.style.width = "100%";
 
-		    	if (window.pageYOffset > 400) {
-		        nav.style.position = "fixed";
-		        nav.style.top = "0px";
-		        nav.style.width = "100%";
+					nav.style.backgroundColor = "#F1F1F1";
+					nav.style.borderRadius = "5px";
+					nav.style.boxShadow = "0px 10px 10px #939393";
+				} else {
+					nav.style.position = "";
+					nav.style.top = "";
 
-		        nav.style.backgroundColor = "#F1F1F1";
-		        nav.style.borderRadius = "5px";
-		        nav.style.boxShadow = "0px 10px 10px #939393";
-		    	} else {
-			    	nav.style.position = "";
-			        nav.style.top = "";
-
-			        nav.style.backgroundColor = "transparent";
-			        nav.style.borderRadius = "none";
-			        nav.style.border = "0px";
-			        nav.style.boxShadow = "";
-      			}
-    		}
+					nav.style.backgroundColor = "transparent";
+					nav.style.borderRadius = "none";
+					nav.style.border = "0px";
+					nav.style.boxShadow = "";
+				}
+			}
     	}
 	});
 
