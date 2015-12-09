1e85cefc1c89d9d6f26e9c5d25026a9ca26e36a3
Your Name <Your email>
2015-12-09 14:36:42 -0800

    Combined two js files together, solved the store page issue

--- js/app.js
+++ js/app.js
@@ -1,10 +1,81 @@
-angular.module('WebApp', [])
+angular.module('WebApp', ['ui.bootstrap'])
+    .factory('productsJSON', function($http) {
+        return $http.get('../data/products.json')
+    })
 
     .factory('usersJSON', function($http) {
         return $http.get('data/userInformation.json');
     })
+
+    .controller('ProductsListCtrl', function($scope,productsJSON){
+        'use strict';
+
+        productsJSON.then(function (results) {
+            $scope.products = results.data;
+
+
+            $scope.categories = _.uniq(_.flatten(_.pluck($scope.products, 'categories')));
+            $scope.coffeeTypes = _.uniq(_.flatten(_.pluck($scope.products, 'coffeeType')));
+
+            $scope.filters = {};
+            $scope.filterCoffee = {};
+            console.log($scope.filterCoffee);
+
+
+        });
+
+    })
+
+    .controller('ProductDetailCtrl', function($scope, $filter, $uibModal, $log, productsJSON) {
+        $scope.closeAlert = function(){
+            $scope.confirmation = !$scope.confirmation;
+
+        };
+        $scope.open = function(product) {
+            $scope.confirmation = false;
+
+            $scope.product = product;
+
+
+            var modalInstance = $uibModal.open({
+                templateUrl:'product-detail.html',
+                size:'md',
+                controller: 'ModalInstanceCtrl',
+                resolve: {
+                    product: function() {
+                        return $scope.product;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedProduct) {
+
+                $scope.selectedProduct = selectedProduct;
+                //console.log($scope.selectedProduct);
+                $scope.confirmation = !$scope.confirmation;
+
+            });
+
+        };
+
+    })
+
+    .controller('ModalInstanceCtrl', function($scope, $uibModalInstance, product) {
+        $scope.product = product;
+        $scope.product.quantity = '1';
+        $scope.close = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+
+        $scope.submit = function() {
+            $uibModalInstance.close($scope.product);
+
+        }
+
+    })
+
     .controller('NavController', function($scope, $http, usersJSON) {
-		'use strict';
+        'use strict';
         usersJSON.then(function(info) {
             $scope.users = info.data;
         });
@@ -13,31 +84,31 @@ angular.module('WebApp', [])
 
         window.onscroll = changePos;
 
-		function changePos() {
-		    var header = document.getElementById("navbar");
-		    if (window.innerWidth > 768) {
-		    	var nav = document.querySelector("nav");
+        function changePos() {
+            var header = document.getElementById("navbar");
+            if (window.innerWidth > 768) {
+                var nav = document.querySelector("nav");
 
-		    	if (window.pageYOffset > 400) {
-		        nav.style.position = "fixed";
-		        nav.style.top = "0px";
-		        nav.style.width = "100%";
+                if (window.pageYOffset > 400) {
+                    nav.style.position = "fixed";
+                    nav.style.top = "0px";
+                    nav.style.width = "100%";
 
-		        nav.style.backgroundColor = "#F1F1F1";
-		        nav.style.borderRadius = "5px";
-		        nav.style.boxShadow = "0px 10px 10px #939393";
-		    	} else {
-			    	nav.style.position = "";
-			        nav.style.top = "";
+                    nav.style.backgroundColor = "#F1F1F1";
+                    nav.style.borderRadius = "5px";
+                    nav.style.boxShadow = "0px 10px 10px #939393";
+                } else {
+                    nav.style.position = "";
+                    nav.style.top = "";
 
-			        nav.style.backgroundColor = "transparent";
-			        nav.style.borderRadius = "none";
-			        nav.style.border = "0px";
-			        nav.style.boxShadow = "";
-      			}
-    		}
-    	}
-	})
+                    nav.style.backgroundColor = "transparent";
+                    nav.style.borderRadius = "none";
+                    nav.style.border = "0px";
+                    nav.style.boxShadow = "";
+                }
+            }
+        }
+    })
 
     .directive('existUser', function() {
         return {
@@ -64,3 +135,13 @@ angular.module('WebApp', [])
             }
         }
     });
+
+
+
+
+
+
+
+
+
+
--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,10 @@
-angular.module('ProductsApp', ['ui.bootstrap'])
-    app.factory('productsJSON', function($http) {
+angular.module('WebApp', ['ui.bootstrap'])
+    .factory('productsJSON', function($http) {
         return $http.get('../data/products.json')
+    })
+
+    .factory('usersJSON', function($http) {
+        return $http.get('data/userInformation.json');
     })
 
     .controller('ProductsListCtrl', function($scope,productsJSON){
@@ -68,8 +72,69 @@ angular.module('ProductsApp', ['ui.bootstrap'])
 
         }
 
-
     })
+
+    .controller('NavController', function($scope, $http, usersJSON) {
+        'use strict';
+        usersJSON.then(function(info) {
+            $scope.users = info.data;
+        });
+
+        $scope.chosenUser = {};
+
+        window.onscroll = changePos;
+
+        function changePos() {
+            var header = document.getElementById("navbar");
+            if (window.innerWidth > 768) {
+                var nav = document.querySelector("nav");
+
+                if (window.pageYOffset > 400) {
+                    nav.style.position = "fixed";
+                    nav.style.top = "0px";
+                    nav.style.width = "100%";
+
+                    nav.style.backgroundColor = "#F1F1F1";
+                    nav.style.borderRadius = "5px";
+                    nav.style.boxShadow = "0px 10px 10px #939393";
+                } else {
+                    nav.style.position = "";
+                    nav.style.top = "";
+
+                    nav.style.backgroundColor = "transparent";
+                    nav.style.borderRadius = "none";
+                    nav.style.border = "0px";
+                    nav.style.boxShadow = "";
+                }
+            }
+        }
+    })
+
+    .directive('existUser', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.existUser = function(value) {
+                    scope.users.forEach(function(user) {
+                        scope.chosenUser = user;
+                        return (value === user.email);
+                    });
+                    return false;
+                }
+            }
+        }
+    })
+
+    .directive('validPassword', function() {
+        return {
+            require:'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.validPassword = function(value) {
+                    return (value === scope.chosenUser.password);
+                }
+            }
+        }
+    });
 
 
 
