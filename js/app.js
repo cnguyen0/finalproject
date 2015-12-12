7c2786ce35fff2eeaaf93b76d6e41f07e1b95543
Mina <minatari@uw.edu>
2015-12-11 18:06:48 -0800

    testing uirouting

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-angular.module('WebApp', ['ui.bootstrap', 'ngAnimate', 'LocalStorageModule'])
+angular.module('WebApp', ['ui.bootstrap', 'ngAnimate', 'LocalStorageModule', 'ui.router', 'angular-uuid'])
     .constant('productKey', 'cart')
 
     .factory('productsJSON', function($http) {
@@ -10,6 +10,29 @@ angular.module('WebApp', ['ui.bootstrap', 'ngAnimate', 'LocalStorageModule'])
     })
     .factory('localStorageJSON', function(localStorageService, storageKey) {
         return localStorageService.get(storageKey) || {items:[]};
+    })
+
+    //configure views for shop to cart
+    .config(function($stateProvider, $urlRouterProvider) { //make sure spelling is exact //declares our various ui-routes and partials, when to show them, etc
+        $stateProvider
+            .state('minaShop', {
+                url: '/minashops', //url will appear in html address
+                templateUrl: 'views/shopview.html', //where is the partial html file that defines this view?
+                controller: 'MinaShopsController' //which controller do I want to use?
+            })
+            .state('minaCart', {
+                url: '/minacarts',
+                templateUrl: 'views/cartview.html',
+                controller: 'MinaCartsController'
+            });
+
+        $urlRouterProvider.otherwise('/minashops'); //just reset that the address ends in this
+    })
+
+    .controller('MinaShopsController', function($scope, contacts) {
+    })
+    .controller('MinaCartsController', function($scope, $stateParams, $state,
+                                                uuid, localStorageService, storageKey, contacts) {
     })
 
     .controller('HomeController', function($scope, $http, usersJSON) {
